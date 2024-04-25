4b6e1c17cba561c4e07b4ff6bccd4beac2b5720d
Theis Jensen <Theis@bmpnj.dk>
2024-04-25 14:32:42 +0200

    fixed functionVisitor

--- cgpl/src/main/java/com/cgpl/visitors/FunctionVisitor.java
+++ cgpl/src/main/java/com/cgpl/visitors/FunctionVisitor.java
@@ -1,19 +1,25 @@
 package com.cgpl.visitors;
 
 import com.cgpl.AST.expressions.Expression;
+import com.cgpl.AST.expressions.Identifier;
 import com.cgpl.AST.instructions.Function;
 import com.cgpl.AST.instructions.Instruction;
 import com.cgpl.CGPLBaseVisitor;
 import com.cgpl.CGPLParser;
 
+import static java.util.stream.Collectors.toList;
+
 import java.util.List;
 
 public class FunctionVisitor extends CGPLBaseVisitor<Instruction> {
     @Override
-    public Instruction visitFunction(CGPLParser.FunctionContext ctx) {
+    public Function visitFunction(CGPLParser.FunctionContext ctx) {
         FunctionBodyVisitor visitor = new FunctionBodyVisitor();
-        ExpressionVisitor expressionVisitor = new ExpressionVisitor();
-        List<Expression> arguments = ctx.IDENTIFIER().stream().map(args -> args.accept(expressionVisitor)).toList();
-        return new Function(ctx.IDENTIFIER(0).getText(), arguments,visitor.visitFunctionBody(ctx.functionBody()));
+        List<Expression> arguments = ctx.IDENTIFIER()
+            .stream()
+            .skip(1)
+            .map(arg -> new Identifier(arg.getText()))
+            .collect(toList());
+        return new Function(ctx.IDENTIFIER(0).getText(), arguments, visitor.visitFunctionBody(ctx.functionBody()));
     }
 }
